168dcccd0fd97f1c4bc66ed9b1461f243ae8f1a6
Jonas Wolf <jonas.wolf@jonaswolf.org>
2023-04-16 14:59:45 +0200

    Infeasible permutations.

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,100 @@
+use std::collections::{BTreeMap, BTreeSet};
 
-use std::collections::{BTreeSet, BTreeMap};
-
-use mathru::algebra::linear::{Vector, Matrix};
-use mathru::algebra::linear::matrix::{Solve, Transpose};
+use itertools::Itertools;
 
 fn solve(input: Vec<(&str, u32)>, solution: &[u32]) -> String {
     // Build symbol table
     let mut symbols = BTreeSet::new();
-    input.iter().for_each(|(i,_)| i.to_uppercase().chars().for_each(|c| { symbols.insert(c); } ));
+    input.iter().for_each(|(i, _)| {
+        i.to_uppercase().chars().for_each(|c| {
+            symbols.insert(c);
+        })
+    });
 
-    let symbol_table = symbols.iter().enumerate().map(|(i,c)| (*c,i)).collect::<BTreeMap<char,usize>>();
+    let symbol_index = symbols
+        .iter()
+        .enumerate()
+        .map(|(i, c)| (*c, i))
+        .collect::<BTreeMap<char, usize>>();
 
     dbg!(&symbols.len());
 
     // B
-    let b = input.iter().map(|(_,b)| *b as f64).collect::<Vec<f64>>();
+    let b = input.iter().map(|(_, b)| *b).collect::<Vec<_>>();
 
-    // Setup A
+    // Setup A (input.len() x symbols.len())
     let mut a = vec![];
-    for (i,_) in input {
+    for (i, _) in input {
         let mut a_in = (0..symbols.len()).map(|_| 0).collect::<Vec<_>>();
         for c in i.to_uppercase().chars() {
-            a_in[symbol_table[&c]] += 1;
+            a_in[symbol_index[&c]] += 1;
         }
         a.push(a_in);
     }
-    // Debug
-    dbg!(a.len());
-    dbg!(a[0].len());
-    dbg!(b.len());
-    assert_eq!(a.len(), b.len());
-    let a0_len = a[0].len();
-    assert!(a.iter().all(|x| x.len() == a0_len));
 
-    // Make matrix A
-    let mut a_matrix: Matrix<f64> = Matrix::zero(a.len(),a[0].len());
-    for row in 0..a.len() {
-        for col in 0..a0_len {
-            a_matrix[[row,col]] = a[row][col] as f64;
+    let sym_vec: Vec<char> = Vec::from_iter(symbols);
+    for p in (0..sym_vec.len()).permutations(sym_vec.len()) {
+        if check_solution(&a, &p, &b) {
+            return solution
+                .iter()
+                .map(|&sym| p.iter().position(|&x| x as u32 == sym).unwrap())
+                .map(|x| sym_vec[x])
+                .collect::<String>()
+                .to_uppercase();
         }
     }
-    dbg!(&a_matrix);
-    let b_vec: Vector<f64> = Vector::new_column(b);
-    dbg!(&b_vec);
 
-    let x = a_matrix.solve(&b_vec);
-    dbg!(&x);
-
-    // FIXME use solution X
-    solution.iter().map(|&sym| symbol_table.iter().find_map(|(&c, &s)| if s == sym.try_into().unwrap() { Some(c)} else {None}).unwrap()).collect::<String>().to_uppercase()
-
-} 
-
-
-
-fn main() {
-
+    "".to_owned()
 }
 
+fn check_solution(a: &Vec<Vec<u32>>, x: &Vec<usize>, b: &Vec<u32>) -> bool {
+    for row in 0..a.len() {
+        if a[row]
+            .iter()
+            .zip(x)
+            .map(|(a, x)| a * (*x as u32))
+            .sum::<u32>()
+            != b[row]
+        {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {}
 
 #[cfg(test)]
 mod test {
     use crate::solve;
 
-
     #[test]
     fn test1() {
-        let input = vec![("BOOGIE", 58), 
-        ("BOSSANOVA",66),
-        ("CHACHACHA",18),
-        ("CHARLESTON",76),
-        ("FLAMENCO",62),
-        ("FOXTROTT",102),
-        ("JIVE",69),
-        ("LAMBADA",29),
-        ("MAMBO",20),
-        ("PASODOBLE",63),
-        ("QUICKSTEP",118),
-        ("ROCKNROLL",88),
-        ("RUMBA",43),
-        ("SAMBA",18),
-        ("SHIMMY",59),
-        ("SIRTAKI",83),
-        ("TANGO",47),
-        ("TWIST",68),
-        ("WALZER",80)];
+        let input = vec![
+            ("BOOGIE", 58),
+            ("BOSSANOVA", 66),
+            ("CHACHACHA", 18),
+            ("CHARLESTON", 76),
+            ("FLAMENCO", 62),
+            ("FOXTROTT", 102),
+            ("JIVE", 69),
+            ("LAMBADA", 29),
+            ("MAMBO", 20),
+            ("PASODOBLE", 63),
+            ("QUICKSTEP", 118),
+            ("ROCKNROLL", 88),
+            ("RUMBA", 43),
+            ("SAMBA", 18),
+            ("SHIMMY", 59),
+            ("SIRTAKI", 83),
+            ("TANGO", 47),
+            ("TWIST", 68),
+            ("WALZER", 80),
+        ];
 
-        let solution = [9,16,7,2,6,8,16,6,22];
+        let solution = [9, 16, 7, 2, 6, 8, 16, 6, 22];
 
         let s = solve(input, &solution);
         assert_eq!(s, "FOXTROTT");
-
     }
 }
